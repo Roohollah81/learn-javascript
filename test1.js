ec288381e5db141cf5461650a4206352b58b7639
Roohollah <roohollah@j.net>
2024-10-21 20:13:13 +0330

    use class

--- test1.js
+++ test1.js
@@ -1,14 +1,11 @@
-const me = {
+class Persion {
     talk() {
         return 'Talking';
     }
 }
 
-const you = {
-    talk() {
-        return 'Talking';
-    }
-}
+const me = new Persion();
+const you = new Persion();
 
 console.log(me.talk());
 console.log(you.talk());

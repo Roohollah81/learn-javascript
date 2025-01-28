81e494c927e48d15235b1a083e0b2b3e739a5e3c
Roohollah <roohollah@j.net>
2025-01-28 19:06:51 +0330

    add Persion class

--- test1.js
+++ test1.js
@@ -1,14 +1,11 @@
-const me = {
+class Person {
     talk() {
         return 'Talking';
     }
 }
 
-const you = {
-    talk() {
-        return 'Talking';
-    }
-}
+const me = new Person(); 
+const you = new Person();
 
 me.talk();
 you.talk();

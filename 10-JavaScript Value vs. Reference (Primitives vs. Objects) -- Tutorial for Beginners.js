65f7d76f0a915436cca7b3018f39761b9df3936b
Roohollah <m.roohollah.v@gmail.com>
2025-02-07 15:24:50 +0330

    episode 10

---
+++ 10-JavaScript Value vs. Reference (Primitives vs. Objects) -- Tutorial for Beginners.js
@@ -0,0 +1,70 @@
+// const a = 1
+// const b = 1
+
+// console.log(a === b)
+
+//--------------------
+
+// const a = [1]
+// const b = [1]
+
+// console.log(a === b)
+
+//--------------------
+
+// const a = {skill: 'Cooking'}
+// const b = {skill: 'Cooking'}
+
+//--------------------
+
+// function incrementAge(age) {
+//     age = age + 1
+// }
+
+// const myAge = 10
+
+// incrementAge(myAge)
+
+// console.log(myAge)
+
+//--------------------
+
+// function incrementAge(user) {
+//     user.age = user.age + 1
+// }
+
+// const me = {age: 10}
+
+// incrementAge(me)
+
+// console.log(me)
+
+//--------------------
+
+// let a = 1
+// let b = a
+
+// console.log(a)
+// console.log(b)
+
+// a = 7
+
+// console.log(b)
+
+//---------------------
+
+let a = {name: 'Sina'}
+let b = a
+
+console.log(a)
+console.log(b)
+
+a.name = 'Qoli'
+
+console.log(a)
+console.log(b)
+
+a = {name: 'CRAIG'}
+
+console.log(a)
+console.log(b)

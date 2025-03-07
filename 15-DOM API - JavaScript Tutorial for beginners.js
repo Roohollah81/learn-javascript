39db629a0979b97e7cc1bbc73c01dba5799dc524
Roohollah <m.roohollah.v@gmail.com>
2025-03-07 20:42:09 +0330

    failed to execute appendChild on Node

--- 15-DOM API - JavaScript Tutorial for beginners.js
+++ 15-DOM API - JavaScript Tutorial for beginners.js
@@ -2,4 +2,7 @@ document.onclick = () => {
     console.log('KHELLO DOM')
 }
 
-document.body.append('MY NEW THINGY');
+document.body.append('MY NEW THINGY')
+
+document.body.appendChild('YET ANOTHER THINGY')
+
